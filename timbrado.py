8945993f9a0a2f117841cb1afd3393d2cb6c22c3
Melanie <meltortolero@gmail.com>
2025-03-05 19:48:53 -0600

    Subiendo xml

--- timbrado.py
+++ timbrado.py
@@ -1,1 +1,9 @@
 # Ejercicio de timbrado
+# Seguridad informática y análisis forense
+# Mel, Leo, Juan y Santi
+
+import xml.etree.ElementTree as ET
+
+# 2. Crear un script que genere un XML CFDI válido con los datos de un emisor y un receptor ficticio
+# (Asegurarse de que el XML cumpla con los requisitos del Anexo 20 del SAT)
+
